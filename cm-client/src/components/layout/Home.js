ec660490ba064ddf7079e47980301ad5415ef0a1
Cian Harrison <cianharry@gmail.com>
2021-04-16 15:31:41 +0100

    Created Navbar and Home react components

---
+++ cm-client/src/components/layout/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div class="text-center">
+            <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+            
+
+            <main role="main" class="inner cover">
+                <h1 class="cover-heading">Checkmates</h1>
+                <p class="lead">Join us today and see how regularly logging your experiences in your digital journal has an positive impact on your feelinsg and the productivity in your life</p>
+                <p class="lead">
+                <a href="#" class="btn btn-lg btn-secondary">Sign Up</a>
+                </p>
+            </main>
+
+            <footer class="mastfoot mt-auto">
+                <div class="inner">
+                <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+                </div>
+            </footer>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ cm-client/src/components/layout/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <header class="masthead mb-auto">
+                <div class="inner">
+                <h3 class="masthead-brand">Checkm8s</h3>
+                <nav class="nav nav-masthead justify-content-center">
+                    <a class="nav-link active" href="#">Dashboard</a>
+                    <a class="nav-link" href="#">Checkins</a>
+                    <a class="nav-link" href="#">Store</a>
+                </nav>
+                </div>
+        </header>
+        
+    )
+}
+
+export default Navbar;
