abc40cf1d6c275da3cc533b9f1ff3fa1ff8bde9b
Cian Harrison <cianharry@gmail.com>
2021-05-11 22:07:04 +0100

    server io port

--- cm-client/src/components/chats/Chats.js
+++ cm-client/src/components/chats/Chats.js
@@ -9,7 +9,7 @@ import './Chats.css'
 
 let CONNECTION_PORT
 if(process.env.NODE_ENV === 'production') {
-    CONNECTION_PORT = `http://localhost:${process.env.PORT}`
+    CONNECTION_PORT = `${process.env.PORT}`
 }
 else {
     CONNECTION_PORT = 'http://localhost:5000'
